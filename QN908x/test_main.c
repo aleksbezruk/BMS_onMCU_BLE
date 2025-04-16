aea945811bfd7be5efd6ff20868c08c5c1b49853
Oleksandr Bezruk <aleksbezruk@gmail.com>
2025-04-16 10:34:33 +0300

    Added skeleton for QN908x VScode project

---
+++ QN908x/test_main.c
@@ -0,0 +1,22 @@
+
+void SystemInit(void);
+
+void SystemInit(void)
+{
+    
+}
+
+int main(void)
+{
+    while(1) {
+        unsigned int i = 0;
+        i++;
+        if (i == 100) {
+            i += 10;
+        }
+    }
+    return 0;
+}
+
+
+/* End of FILE */
